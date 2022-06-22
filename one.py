4d337929274ac782aa71a754449a9d46e9f82e81
mrjudyabraham <mrjudyabraham@gmail.com>
2022-06-23 00:03:27 +0530

    some things i did

--- one.py
+++ one.py
@@ -1,42 +1,61 @@
+from unittest import result
 import cv2
 from cv2 import IMWRITE_PNG_BILEVEL
 import numpy as np
 import face_recognition
+import os
 
-path="/home/madj/Documents/Github/Face-Recognition/"
+def decode(path):
+    img = face_recognition.load_image_file(path)
+    img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    faceLoc = face_recognition.face_locations(img)[0]
+    faceEnc = face_recognition.face_encodings(img)[0]
+    return faceEnc,faceLoc,img
 
-imgElon = face_recognition.load_image_file(path+'musk.jpg')
-imgElon = cv2.cvtColor(imgElon,cv2.COLOR_BGR2RGB)
-img = face_recognition.load_image_file(path+'musk2.jpg')
-img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-imgbill = face_recognition.load_image_file(path+'billgates.jpg')
-imgbill = cv2.cvtColor(imgbill,cv2.COLOR_BGR2RGB)
+def compare(known,unknown):
+    knownEnc=known[0]
+    unknownEnc=unknown[0]
+ #  print(knownEnc)
+ #  print(unknownEnc)
+    result=face_recognition.compare_faces([knownEnc],unknownEnc)
+    distance=face_recognition.face_distance([knownEnc],unknownEnc)
+    return result,distance
 
-faceLoc=face_recognition.face_locations(imgElon)[0]
-encodeElon=face_recognition.face_encodings(imgElon)[0]
-faceLoc1=face_recognition.face_locations(img)[0]
-encodeImg=face_recognition.face_encodings(img)[0]
-faceLoc2=face_recognition.face_locations(imgbill)[0]
-encodeBill=face_recognition.face_encodings(imgbill)[0]
-
-cv2.rectangle(imgElon,(faceLoc[3],faceLoc[0]),(faceLoc[1],faceLoc[2]),(255,0,255),2)
-cv2.rectangle(img,(faceLoc1[3],faceLoc1[0]),(faceLoc1[1],faceLoc1[2]),(0,255,0),2)
-cv2.rectangle(imgbill,(faceLoc2[3],faceLoc2[0]),(faceLoc2[1],faceLoc2[2]),(255,0,0),2)
-  
-results = face_recognition.compare_faces([encodeElon],encodeImg)
-faceDis=face_recognition.face_distance([encodeElon],encodeImg)
-
-print(results, faceDis)
-
-name="Elon Musk"
-if results[0]==True:
-    cv2.imshow('Identified as '+name, img)
+def draw_rect(data):
+    img=data[2]
+    faceLoc=data[1]
+    cv2.rectangle(img,(faceLoc[3],faceLoc[0]),(faceLoc[1],faceLoc[2]),(0,255,0),2)
+    cv2.imshow('Image',img)
     cv2.waitKey(0)
 
 
+data_path="/home/madj/Documents/Github/Face-Recognition/data/"
+identify_path="/home/madj/Documents/Github/Face-Recognition/identify/"
+files_unknown=os.listdir(identify_path)
+files_known=os.listdir(data_path)
 
-#cv2.imshow('Elon Musk',imgElon)
-#cv2.imshow('Elon ',img)
-#cv2.imshow('Bill',imgbill)
-#cv2.waitKey(0)
+
+known_data = []
+unknown_data = []
+for i in files_known:
+    path=data_path+i
+    known_data.append(decode(path))
+
+for j in files_unknown:
+    path=identify_path+j
+    unknown_data.append(decode(path))
+
+#print(known_data)
+#print(unknown_data)
+
+for i in known_data:
+    for j in unknown_data:
+        output=compare(i,j)
+        print(output)
+
+'''
+cv2.rectangle(img,(faceLoc[3],faceLoc[0]),(faceLoc[1],faceLoc[2]),(0,255,0),2)
+cv2.imshow('Elon ',img)
+cv2.waitKey(0)
+'''
 
